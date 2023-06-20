ae110df8f39b5189bf79a9d24dc223d07f56a537
Timothy Varjan <tim.varjan@gmail.com>
2023-06-20 19:36:43 -0300

    initial commit

---
+++ testsite.js
@@ -0,0 +1,13 @@
+"use strict";
+
+
+    // Function to show the sign-up modal after 4 seconds
+    function showSignupModal() {
+    $('#signupModal').modal('show');
+}
+
+    // Delay the execution of the showSignupModal function by 4 seconds
+    setTimeout(showSignupModal, 4000);
+
+
+
